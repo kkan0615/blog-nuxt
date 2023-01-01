6145dca2301abc9b6cf3e353e57784bf90e08fec
kkan0615 <kkan0615@gmail.com>
2022-12-31 21:30:55 -0500

    :zap: Feat: update google analytics

--- plugins/vue-gtag.client.ts
+++ plugins/vue-gtag.client.ts
@@ -3,8 +3,8 @@ import { defineNuxtPlugin } from 'nuxt/app'
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(VueGtag, {
-    property: {
-      id: process.env.NUXT_PUBLIC_GOOGLE_ANALYTICS_ID
-    }
-  })
+    config: {
+      id: process.env.NUXT_PUBLIC_GOOGLE_ANALYTICS_ID,
+    },
+  }, nuxtApp.$router)
 })
