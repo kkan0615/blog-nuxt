5d734cdc1fdecf99c307a4322443286398e59ca3
kkan0615 <kkan0615@gmail.com>
2022-12-31 22:08:33 -0500

    :hammer: Fix: Fix and test: Google analytics 4

--- app.vue
+++ app.vue
@@ -5,13 +5,14 @@
 </template>
 <script setup lang="ts">
 import './libs/dayjs'
+const { t } = useI18n()
 
 // SEO
 useHead({
-  title: 'Nuxt-study',
+  title: t('seo.title'),
   meta: [
-    { name: 'application-name', content: 'Nuxt-study' },
-    { name: 'description', content: 'It is Nuxt study website' },
+    { name: 'application-name', content: t('seo.applicationName') },
+    { name: 'description', content: t('seo.description') },
     { name: 'author', content: 'Requiem' },
     { name: 'publisher', content: 'Requiem' },
     { name: 'robots', content: 'index, follow' }
