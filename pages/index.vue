95c98228da2e62e7ea2db4daa40f4f46d5b16004
kkan0615 <kkan0615@gmail.com>
2023-01-18 11:33:20 -0500

    :zap: Feat: udate

--- pages/index.vue
+++ pages/index.vue
@@ -7,30 +7,47 @@ const { t } = useI18n()
 
 </script>
 <template>
-  <div class="hero h-full">
-    <div class="hero-content max-w-4xl lg:space-x-12 flex-col lg:flex-row-reverse">
-      <NuxtImg
-        src="/assets/home/hero.jpg"
-        alt="left-image"
-        class="max-w-sm rounded-full shadow-2xl animate__animated animate__fadeIn"
-        height="250"
-        width="250"
-      />
-      <div>
-        <h1 class="text-5xl font-bold animate__animated animate__bounce">
-          {{ t('views.home.title') }}
-        </h1>
-        <p class="py-6 text-xl">
-          {{ t('views.home.description') }}
-        </p>
-        <NuxtLink
-          :to="localePath('/contact')"
-        >
-          <button class="btn btn-primary">
-            {{ t('menus.contact') }}
-          </button>
-        </NuxtLink>
-      </div>
+  <!--  <div class="hero h-full">-->
+  <!--    <div class="hero-content max-w-4xl lg:space-x-12 flex-col lg:flex-row-reverse">-->
+  <!--      <NuxtImg-->
+  <!--        src="/assets/home/hero.jpg"-->
+  <!--        alt="left-image"-->
+  <!--        class="max-w-sm rounded-full shadow-2xl animate__animated animate__fadeIn"-->
+  <!--        height="250"-->
+  <!--        width="250"-->
+  <!--      />-->
+  <!--      <div>-->
+  <!--        <h1 class="text-5xl font-bold animate__animated animate__bounce">-->
+  <!--          {{ t('views.home.title') }}-->
+  <!--        </h1>-->
+  <!--        <p class="py-6 text-xl">-->
+  <!--          {{ t('views.home.description') }}-->
+  <!--        </p>-->
+  <!--        <NuxtLink-->
+  <!--          :to="localePath('/contact')"-->
+  <!--        >-->
+  <!--          <button class="btn btn-primary">-->
+  <!--            {{ t('menus.contact') }}-->
+  <!--          </button>-->
+  <!--        </NuxtLink>-->
+  <!--      </div>-->
+  <!--    </div>-->
+  <!--  </div>-->
+  <div class="hero h-full w-full">
+    <div class="max-w-4xl">
+      <h1 class="text-7xl font-bold animate__animated animate__bounce">
+        {{ t('views.home.title') }}
+      </h1>
+      <p class="py-6 text-2xl">
+        {{ t('views.home.description') }}
+      </p>
+      <NuxtLink
+        :to="localePath('/contact')"
+      >
+        <button class="btn btn-primary">
+          {{ t('menus.contact') }}
+        </button>
+      </NuxtLink>
     </div>
   </div>
 </template>
