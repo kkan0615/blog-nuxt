d9c199d33e12103e7d7b859412695bdc4d84e1f5
kkan0615 <kkan0615@gmail.com>
2023-01-26 20:30:33 -0500

    :zap: Feat version update and change vue-gtag

--- plugins/vue-gtag.client.ts
+++ plugins/vue-gtag.client.ts
@@ -1,25 +1,25 @@
 // import { defineNuxtPlugin } from 'nuxt/app'
-// import VueGtag from 'vue-gtag'
-// import { useRuntimeConfig } from '#app'
-//
-// export default defineNuxtPlugin((nuxtApp) => {
-//   const config = useRuntimeConfig()
-//   nuxtApp.vueApp.use(VueGtag, {
-//     config: {
-//       id: config.GOOGLE_ANALYTICS_ID,
-//     },
-//   }, nuxtApp.$router)
-// })
-
-//@MEMO: Offciail website using following code
-import VueGtag from 'vue-gtag-next'
+import VueGtag from 'vue-gtag'
+import { useRuntimeConfig } from '#app'
 
 export default defineNuxtPlugin((nuxtApp) => {
   const config = useRuntimeConfig()
-
   nuxtApp.vueApp.use(VueGtag, {
-    property: {
-      id: config.GOOGLE_ANALYTICS_ID
-    }
-  })
+    config: {
+      id: config.GOOGLE_ANALYTICS_ID,
+    },
+  }, nuxtApp.$router)
 })
+
+//@MEMO: Offciail website using following code
+// import VueGtag from 'vue-gtag-next'
+//
+// export default defineNuxtPlugin((nuxtApp) => {
+//   const config = useRuntimeConfig()
+//
+//   nuxtApp.vueApp.use(VueGtag, {
+//     property: {
+//       id: config.GOOGLE_ANALYTICS_ID
+//     }
+//   })
+// })
