7bf0a6effe68170f5407752ba3e0539e65506811
kkan0615 <kkan0615@gmail.com>
2023-02-22 21:15:24 -0500

    :zap: Feat: update sitemap

--- server/routes/sitemap.xml.ts
+++ server/routes/sitemap.xml.ts
@@ -1,20 +1,38 @@
 import { serverQueryContent } from '#content/server'
 import { SitemapStream, streamToPromise } from 'sitemap'
+import { LocaleCodeList } from '~/types/locale'
+
+const paths = [
+  '/',
+  '/blogs',
+  '/showcases',
+  '/contact',
+  '/contact/requests'
+]
 
 export default defineEventHandler(async (event) => {
   const runtimeConfig = useRuntimeConfig()
 
   // Fetch all documents
-  const docs = await serverQueryContent(event).find()
   const sitemap = new SitemapStream({
     hostname: runtimeConfig.NUXT_PUBLIC_BASE_URL
   })
+  LocaleCodeList.map(localeCodeEl => {
+    paths.map(pathEl => {
+      sitemap.write({
+        url: `${localeCodeEl}${pathEl}`
+      })
+    })
+  })
+  // Add documents about blog and showcase
+  const docs = await serverQueryContent(event).find()
   for (const doc of docs) {
     sitemap.write({
       url: doc._path,
       changefreq: 'monthly'
     })
   }
+
   sitemap.end()
   return streamToPromise(sitemap)
 })
