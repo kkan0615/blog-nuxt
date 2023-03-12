add5369267256cb2a8ce82a469a90a8bd0f490e0
kkan0615 <kkan0615@gmail.com>
2023-03-11 20:27:45 -0500

    :zap: Feat: change cookie icon

--- components/CookiePopup.vue
+++ components/CookiePopup.vue
@@ -12,20 +12,15 @@ const handleAccept = () => {
 <template>
   <div
     v-if="visible"
-    class="fixed bottom-2 right-0 z-50 alert shadow-lg max-w-lg"
+    class="animate__animated animate__fadeIn fixed bottom-2 right-2 z-50 alert shadow-lg max-w-4xl"
   >
     <div>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        class="stroke-info flex-shrink-0 w-6 h-6"
-      ><path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-        d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-      /></svg>
+      <NuxtImg
+        class="mr-2"
+        src="/assets/cookie-32x32.png"
+        width="32px"
+        height="32px"
+      />
       <span>
         {{ t('commons.messages.cookieAccept') }}
       </span>
