4ded8628518e8b124c3c43b9a49fee81c06a7d70
kkan0615 <kkan0615@gmail.com>
2023-04-05 21:02:48 -0400

    :art: update style

--- components/showcases/list/ShowcaseCard.vue
+++ components/showcases/list/ShowcaseCard.vue
@@ -22,7 +22,7 @@ const props = withDefaults(defineProps<Props>(), {
 
 </script>
 <template>
-  <div class="card rounded bg-base-200 hover:scale-105 transition ease-in-out duration-300">
+  <div class="card rounded bg-base-200/50 hover:scale-105 transition ease-in-out duration-300">
     <NuxtLink
       class="flex flex-col h-full"
       :to="localePath(blog._path)"
