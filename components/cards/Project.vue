7b67641acadcca271813567a4f0ec702d182cab7
kkan0615 <kkan0615@gmail.com>
2024-04-15 23:25:59 -0400

    :zap: Feat: update package version

---
+++ components/cards/Project.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+defineProps<{
+  project: any // Project
+}>()
+
+const localePath = useLocalePath()
+</script>
+
+<template>
+  <NuxtLink
+    class="h-full"
+    :to="localePath(project.id)"
+  >
+    Project
+  </NuxtLink>
+</template>
+
+<style>
+
+</style>
